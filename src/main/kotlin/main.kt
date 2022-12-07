9d76a7dabb5d02a1970aade2097c3db79f446266
Nik Ze <zebolovnik@yandex.ru>
2022-12-07 14:14:07 +1000

    add .toInt to resultPersent

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,12 @@
 package ru.netology
 
 fun main() {
-    val amount = 10000 // суммаПеревода
+    val amount = 50009 // суммаПеревода
     val percent = 0.75 // процентКомиссии 0,75%
     val minRate = 35 // минимальнаяКомиссия 35руб
 //    resultPercent = итоговаяСуммаКомиссии
 
     val resultPercent = amount * (percent / 100) // расчет суммы комиссии
     println("Сумма комиссии составит: " +
-            (if (resultPercent < minRate) minRate else resultPercent) + " руб.")
+            (if (resultPercent < minRate) minRate else resultPercent.toInt()) + " руб.")
 }
